253457d7460fd68daca0241afc6dfecfa6fbe35b
maomao0819 <maomao.08190@gmail.com>
2022-12-25 20:13:31 +0800

    add private data

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -f data ]; then
-    gdown https://drive.google.com/uc?id=16PPRUpJVrtAlg65KoJsfPVAmiWp_Kw2C -O data.zip
+    gdown https://drive.google.com/uc?id=10SO0P5Ompp2fck5fhJXL2pDgCIgUnPNO -O data.zip
     unzip data.zip
 fi
